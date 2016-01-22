4ea3c428c893a058c79f914e8ed4ffc8cb783f8e
alvaro <alraled@hotmail.es>
2016-01-22 14:03:23 +0100

    Canvas

---
+++ Canvas/canvas.js
@@ -0,0 +1,15 @@
+﻿var canvas = document.getElementById("micanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#ccff66";
+ctx.fillRect(10, 30, 100, 100);
+
+ctx.strokeStyle = "#66aa66";
+ctx.moveTo(50, 30);
+ctx.lineTo(250, 197);
+ctx.stroke();
+
+ctx.strokeStyle = "red";
+ctx.font = "40px Arial";
+ctx.strokeText("¡¡¡ Hola Mundo !!!",50,50);
+
